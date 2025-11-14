6543de3b42581b62713843718db5ef564d5ee260
Misha908989 <131542265+Misha908989@users.noreply.github.com>
2025-11-14 19:04:14 +0200

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
@@ -6,3 +7,13 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+=======
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+>>>>>>> 05b61324031fcad608b1061194830fb43e787086
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
@@ -11,3 +12,18 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+=======
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+
+    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+
+    return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+>>>>>>> 05b61324031fcad608b1061194830fb43e787086
